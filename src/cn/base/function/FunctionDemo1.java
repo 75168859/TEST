7d5a504aced47228357aac4c5e960e6f40f3962b
= <=>
2015-12-02 23:01:57 +0800

    笔记

---
+++ src/cn/base/function/FunctionDemo1.java
@@ -0,0 +1,20 @@
+package cn.base.function;
+
+public class FunctionDemo1 {
+	int a =1;
+	public static void main(String[] args) { 
+		int a = 1;
+		int b = 2;
+//		System.out.println(add(1,2));
+		System.out.println(a);
+	}
+	
+	
+	/*public static int add(int a,int b){
+		return a + b;
+	}*/
+	public static void add(int a,int b){
+		System.out.println(a+b);
+		return;
+	}
+}
