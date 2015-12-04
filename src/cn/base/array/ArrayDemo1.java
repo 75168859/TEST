33685d8d8ae1bdc0ca1b09a4235fd5842659d462
75168859 <75168859@qq.com>
2015-12-04 10:53:24 +0800

    数组学习

--- src/cn/base/array/ArrayDemo1.java
+++ src/cn/base/array/ArrayDemo1.java
@@ -12,5 +12,15 @@ public class ArrayDemo1 {
 //		System.out.println(i);
 		new ArrayDemo1().sop();
 		System.out.println("---" + "\u0000" + "---");
+		int[] arr = new int[3];//格式1
+		System.out.println(arr);  ///  [I@ae3865e    [I 代表实体类型：数组型实体，int类型的
+		
+		int[] arr2 = new int[]{2,34,55,23};//格式2
+		
+		int[] arr3 = {23,23,444,45}; //静态初始化方式
+		
+		
+		System.out.println(arr3);
+		
 	}
 }
