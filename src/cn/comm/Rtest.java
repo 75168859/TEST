fb18110132f17b9296c5d07154731ab81169fd2f
75168859 <75168859@qq.com>
2015-12-09 08:46:59 +0800

    20151209

--- src/cn/comm/Rtest.java
+++ src/cn/comm/Rtest.java
@@ -3,13 +3,12 @@ package cn.comm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 
 public class Rtest {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = "";
-		while((str = br.readLine()) != null){
-			System.out.println(str);
-		}
+		String s = "";
+		BigDecimal b = new BigDecimal("20").add(new BigDecimal(s));
+		System.out.println(b);
 	}
 }
